7a88a87553050fd18a9caef960be5273344c8d39
andres-fr <fr_andres@msn.com>
2023-12-23 19:23:55 +0100

    eigenvalue recovery and truncation works

--- pytorch_ssvd/ssvd.py
+++ pytorch_ssvd/ssvd.py
@@ -6,6 +6,10 @@
 """
 
 import math
+import torch
+
+#
+from .sketching import SSRFT
 
 
 # ##############################################################################
@@ -27,7 +31,58 @@ def a_priori_hyperparams(
     mn4a = m + n + 4 * alpha
     budget_root = 16 * (memory_budget - alpha**2)
     #
-    k = math.floor((1 / 8) * (math.sqrt(mn4a**2 + budget_root) - mn4a))
-    s = math.floor(math.sqrt(memory_budget - k * (m + n)))
+    outer_dim = math.floor(
+        (1 / 8) * (math.sqrt(mn4a**2 + budget_root) - mn4a)
+    )
+    core_dim = math.floor(math.sqrt(memory_budget - outer_dim * (m + n)))
     #
-    return k, s
+    return outer_dim, core_dim
+
+
+# ##############################################################################
+# # SSVD
+# ##############################################################################
+def ssvd(
+    matrix,
+    matrix_device,
+    matrix_dtype,
+    outer_dim,
+    core_dim,
+    seed=0b1110101001010101011,
+):
+    """ """
+    h, w = matrix.shape
+    # instantiate random sketching matrices
+    left_outer_ssrft = SSRFT((outer_dim, h), seed=seed)
+    right_outer_ssrft = SSRFT((outer_dim, w), seed=seed + 1)
+    left_core_ssrft = SSRFT((core_dim, h), seed=seed + 2)
+    right_core_ssrft = SSRFT((core_dim, w), seed=seed + 3)
+    # perform random measurements
+    eye = torch.eye(outer_dim, dtype=matrix_dtype).to(matrix_device)
+    lo_measurements = (eye @ left_outer_ssrft) @ matrix
+    ro_measurements = matrix @ (eye @ right_outer_ssrft).T
+    #
+    eye = torch.eye(core_dim, dtype=matrix_dtype).to(matrix_device)
+    core_measurements = (
+        (eye @ left_core_ssrft) @ matrix @ (eye @ right_core_ssrft).T
+    )
+    del eye
+    # QR decompositions of outer measurements
+    lo_Q = torch.linalg.qr(lo_measurements.T)[0].T
+    del lo_measurements
+    ro_Q = torch.linalg.qr(ro_measurements)[0]
+    del ro_measurements
+    # Solve core matrix to yield initial approximation
+    left_core = left_core_ssrft @ ro_Q
+    right_core = right_core_ssrft @ lo_Q.T
+    core = torch.linalg.lstsq(left_core, core_measurements).solution
+    core = torch.linalg.lstsq(right_core, core.T).solution
+    # SVD of core matrix for truncated approximation
+    core_U, core_S, core_Vt = torch.linalg.svd(core)
+    #
+    return lo_Q.T, core_U, core_S, core_Vt, ro_Q.T
+
+
+def truncate_core(core_U, core_S, core_Vt, k):
+    """ """
+    return core_U[:, :k], core_S[:k], core_Vt[:k, :]
--- test.py
+++ test.py
@@ -10,10 +10,8 @@
 import torch
 
 from pytorch_ssvd.synthmat import SynthMat
-from pytorch_ssvd.ssvd import a_priori_hyperparams
 from pytorch_ssvd.sketching import SSRFT
-from pytorch_ssvd.cg import CG
-
+from pytorch_ssvd.ssvd import a_priori_hyperparams, ssvd, truncate_core
 
 import matplotlib.pyplot as plt
 
@@ -23,11 +21,11 @@ import matplotlib.pyplot as plt
 # ##############################################################################
 DEVICE = "cuda"
 SEED = 0b1110101001010101011
-IN, OUT, DTYPE = 1000, 500, torch.float64
+IN, OUT, DTYPE = 1000, 1000, torch.float64
 RANK = 10
 #
 MEMORY_BUDGET = 100_000
-OUTER_K, CORE_K = a_priori_hyperparams((OUT, IN), MEMORY_BUDGET)
+OUTER_DIM, CORE_DIM = a_priori_hyperparams((OUT, IN), MEMORY_BUDGET)
 
 
 # ##############################################################################
@@ -37,59 +35,33 @@ OUTER_K, CORE_K = a_priori_hyperparams((OUT, IN), MEMORY_BUDGET)
 # 1. create test matrix and compute its hard SVD
 # mm = SynthMat.lowrank_noise((OUT, IN), rank=10, snr=1e-1, device="cuda")
 # em = SynthMat.exp_decay((OUT, IN), rank=10, decay=0.1, symmetric=True)
-# mat = SynthMat.exp_decay(
-#     (OUT, IN), rank=RANK, decay=0.1, symmetric=False, dtype=DTYPE, device=DEVICE
-# )
-mat = SynthMat.poly_decay(
-    (OUT, IN), rank=RANK, decay=2, symmetric=False, dtype=DTYPE, device=DEVICE
+mat = SynthMat.exp_decay(
+    (OUT, IN), rank=RANK, decay=0.1, symmetric=True, dtype=DTYPE, device=DEVICE
 )
+# mat = SynthMat.poly_decay(
+#     (OUT, IN), rank=RANK, decay=2, symmetric=False, dtype=DTYPE, device=DEVICE
+# )
 U, S, Vt = torch.linalg.svd(mat, full_matrices=False)
 # plt.clf(); plt.imshow(em.cpu()); plt.show()
 # plt.clf(); plt.loglog(S.cpu()); plt.show()
 
 
-# 2. draw random measurements and perform QR decomposition
-left_outer_ssrft = SSRFT((OUTER_K, OUT), seed=SEED)
-right_outer_ssrft = SSRFT((OUTER_K, IN), seed=SEED + 1)
-left_core_ssrft = SSRFT((CORE_K, OUT), seed=SEED + 2)
-right_core_ssrft = SSRFT((CORE_K, IN), seed=SEED + 3)
-#
-lo_measurements = (
-    torch.eye(OUTER_K, dtype=DTYPE).to(DEVICE) @ left_outer_ssrft
-) @ mat
-ro_measurements = (
-    mat @ (torch.eye(OUTER_K, dtype=DTYPE).to(DEVICE) @ right_outer_ssrft).T
+lo_Qt, core_U, core_S, core_Vt, ro_Qt = ssvd(
+    mat, DEVICE, DTYPE, OUTER_DIM, CORE_DIM, SEED
 )
-core_measurements = (
-    (torch.eye(CORE_K, dtype=DTYPE).to(DEVICE) @ left_core_ssrft)
-    @ mat
-    @ (torch.eye(CORE_K, dtype=DTYPE).to(DEVICE) @ right_core_ssrft).T
-)
-
-# 3. Perform QR decompositions of outer measurements
-lo_Q = torch.linalg.qr(lo_measurements.T)[0].T
-ro_Q = torch.linalg.qr(ro_measurements)[0]
-
-
-# 4. Solve core matrix to yield initial approximation
-left_core = left_core_ssrft @ ro_Q
-right_core = right_core_ssrft @ lo_Q.T
-core = torch.linalg.lstsq(left_core, core_measurements).solution
-core = torch.linalg.lstsq(right_core, core.T).solution
-
-# 5. SVD of core matrix and truncated approximation
-core_U, core_S, core_Vt = torch.linalg.svd(core)
+mat_recons = lo_Qt @ core_U @ torch.diag(core_S) @ core_Vt @ ro_Qt
 # plt.clf(); plt.plot(core_S.cpu()); plt.show()
-
-"""
-a) take SVD, and grab top r pairs
-b) Multiply Q matrices by truncated SVD ones, we are done.
-
-See algorithm 4.4 in page 13
-
-"""
+# plt.clf(); plt.imshow(mat.cpu()); plt.show()
 
 
+trunc_U, trunc_S, trunc_Vt = truncate_core(core_U, core_S, core_Vt, 10)
+
+mat10 = U[:, :10] @ torch.diag(S[:10]) @ Vt[:10, :]
+mat10_recons = lo_Qt @ trunc_U @ torch.diag(trunc_S) @ trunc_Vt @ ro_Qt
+
+
+print(torch.dist(mat, mat_recons))
+print(torch.dist(mat10, mat10_recons))
 breakpoint()
 
 # 6. A posteriori precision and rank estimation
